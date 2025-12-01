93e3bd43b508fe6f34906eec67c026fb133ff6b7
omer gokturk <omergokturktc@gmail.com>
2025-12-01 13:08:47 +0300

    feat: tab ikonlari ve ozel header stillendirmesi (Bolum 5)

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { Text, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import HomeScreen from './screens/HomeScreens';
 import DetailsScreen from './screens/DetailsScreen';
@@ -12,7 +13,12 @@ const Tab = createBottomTabNavigator();
 
 function HomeStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: 'tomato' }, // Header Arkaplan
+        headerTintColor: '#fff', // Header Yazı Rengi
+      }}
+    >
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Details" component={DetailsScreen} />
     </Stack.Navigator>
@@ -22,7 +28,24 @@ function HomeStack() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Explore') {
+              iconName = focused ? 'map' : 'map-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
         <Tab.Screen name="Explore" component={HomeStack} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
